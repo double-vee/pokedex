4c8052198a6cd2fe2c9a6c79bf930edea9cebd89
Weronika Kansy <weronika.kansy@gmail.com>
2021-03-10 14:25:11 +0100

    Page children centered with flex

--- src/components/page.js
+++ src/components/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Page = ({children, className}) => {
   return (
-    <div className={`mt-12 p-8 bg-red-500 rounded shadow-xl ${className ? className : ''}`}>
+    <div className={`flex flex-col items-center mt-12 p-8 bg-red-500 rounded shadow-xl ${className ? className : ''}`}>
       {children}
     </div>
   )
