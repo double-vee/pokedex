c60c6d4a50cf2aff90731cc277290acbd256d904
Weronika Kansy <weronika.kansy@gmail.com>
2024-03-24 02:29:03 +0100

    Refactored pokemon details

--- src/views/pokemons/pokemon-details.js
+++ src/views/pokemons/pokemon-details.js
@@ -1,20 +1,22 @@
-import { useState, useEffect } from "react";
-import { useLocalStorage } from "../../hooks/useLocalStorage";
-import { Page } from "../../components/page";
-import { Title } from "../../components/title";
+import { useState, useEffect } from 'react';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { Page } from '../../components/page';
+import { Title } from '../../components/title';
 
-export const PokemonDetails = (props) => {
-  const [pokemonData, setPokemonData] = useState({});
+export const PokemonDetails = ({ match: { params } }) => {
+  const URL = `https://pokeapi.co/api/v2/pokemon/${params.id}`;
+
+  const [pokemonData, setPokemonData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [favPokemons, setFavPokemons] = useLocalStorage("pokemons", []);
+  const [favPokemons, setFavPokemons] = useLocalStorage('pokemons', []);
   const [alert, setAlert] = useState(null);
 
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${props.match.params.id}`)
+    fetch(URL)
       .then((response) => {
         if (!response.ok) {
-          throw Error("Could not fetch data");
+          throw Error('Could not fetch data');
         }
         return response.json();
       })
@@ -27,7 +29,7 @@ export const PokemonDetails = (props) => {
         setError(error.message);
         setLoading(false);
       });
-  }, []);
+  }, [URL]);
 
   const addToFav = () => {
     if (favPokemons.find((pokemon) => pokemon.id === pokemonData.id)) {
@@ -46,58 +48,53 @@ export const PokemonDetails = (props) => {
     setFavPokemons([...favPokemons, pokemonData]);
   };
 
-  if (loading) {
-    return (
-      <Page>
-        <Title>Pokemon details</Title>
+  return (
+    <Page>
+      <Title>Pokemon details</Title>
+
+      {loading && (
         <p className="poke-font font-semibold text-center text-white">
           Loading...
         </p>
-      </Page>
-    );
-  }
+      )}
 
-  if (error) {
-    return (
-      <Page>
-        <Title>Pokemon details</Title>
+      {error && (
         <div className="poke-font font-semibold leading-relaxed text-center text-red-900">
           <p>Something went wrong :(</p>
           <p>{error}</p>
         </div>
-      </Page>
-    );
-  }
-
-  return (
-    <Page>
-      <Title>Pokemon details</Title>
-      <figure className="max-w-md sm:mt-4 mb-8 bg-gray-100 rounded-xl p-4">
-        <img
-          className="w-32 h-32 rounded-full mx-auto object-cover bg-white"
-          src={pokemonData.sprites.front_default}
-          alt={pokemonData.name}
-        />
-        <figcaption className="pt-4 text-center font-medium">
-          <div className="text-cyan-600 uppercase">
-            #{pokemonData.id} {pokemonData.name}
-          </div>
-          <div className="text-gray-500">
-            {pokemonData.types.map(({ type }) => type.name).join(", ")}
-          </div>
-        </figcaption>
-      </figure>
-      {alert && (
-        <p className="poke-font mb-8 font-semibold leading-relaxed text-center text-red-900">
-          {alert}
-        </p>
       )}
-      <button
-        className="poke-font py-2 px-4 bg-white hover:bg-red-100 rounded text-red-500 font-semibold text-base uppercase"
-        onClick={addToFav}
-      >
-        Add to favorites
-      </button>
+
+      {pokemonData !== null && (
+        <>
+          <figure className="max-w-md sm:mt-4 mb-8 bg-gray-100 rounded-xl p-4">
+            <img
+              className="w-32 h-32 rounded-full mx-auto object-cover bg-white"
+              src={pokemonData.sprites.front_default}
+              alt={pokemonData.name}
+            />
+            <figcaption className="pt-4 text-center font-medium">
+              <div className="text-cyan-600 uppercase">
+                #{pokemonData.id} {pokemonData.name}
+              </div>
+              <div className="text-gray-500">
+                {pokemonData.types.map(({ type }) => type.name).join(', ')}
+              </div>
+            </figcaption>
+          </figure>
+          {alert && (
+            <p className="poke-font mb-8 font-semibold leading-relaxed text-center text-red-900">
+              {alert}
+            </p>
+          )}
+          <button
+            className="poke-font py-2 px-4 bg-white hover:bg-red-100 rounded text-red-500 font-semibold text-base uppercase"
+            onClick={addToFav}
+          >
+            Add to favorites
+          </button>
+        </>
+      )}
     </Page>
   );
 };
